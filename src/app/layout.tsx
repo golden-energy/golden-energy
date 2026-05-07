8fa08bb12846b3476ed10015c076a912abcbb359
golden-energy <goldenenenergy@gmail.com>
2026-05-07 18:02:36 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
   category: "Electrical Supply & Services",
 
   alternates: {
-    canonical: "/",
+    canonical: "https://goldenenerggy.com/",
   },
 
   openGraph: {
