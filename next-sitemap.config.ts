c4e35666363e8f70e8f68e42baa215cb5b698e64
Moaath Ibrahim <goldenenenergy@gmail.com>
2026-05-07 17:07:25 +0300

    editing sitemap

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -1,4 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: 'https://www.goldenenerggy.com/',
     generateRobotsTxt: true,
+    sitemapSize: 5000,
+    changefreq: 'weekly',
+    priority: 0.8,
 }
