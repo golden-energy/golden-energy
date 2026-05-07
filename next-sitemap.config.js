cfcefd426e6e0c2ade9066aec2c27e503126c3bf
Moaath Ibrahim <goldenenenergy@gmail.com>
2026-05-07 17:26:04 +0300

    fixed

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,16 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.goldenenerggy.com/',
-    generateRobotsTxt: true,
-    sitemapSize: 5000,
-    changefreq: 'weekly',
-    priority: 0.8,
+  siteUrl: 'https://www.goldenenerggy.com/',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'weekly',
+  priority: 0.8,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
 }
